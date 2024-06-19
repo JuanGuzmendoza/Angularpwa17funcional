c18ef7c7349de2d79929f4e58cb55690961829ec
yamid <yamithmass02@gmail.com>
2024-06-19 14:46:05 -0500

    Pwa ahora si funcionando

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -7,6 +7,7 @@ import { provideServiceWorker } from '@angular/service-worker';
 export const appConfig: ApplicationConfig = {
   providers: [provideRouter(routes), provideServiceWorker('ngsw-worker.js', {
         enabled: !isDevMode(),
-        registrationStrategy: 'registerWhenStable:30000'
+        registrationStrategy: 'registerWhenStable:30000',
+        scope:'/pwa'
     })]
 };
